c54b60a61e48208defb44ed98e91eaa12bdf2ca9
zunema <yxxa.abc@gmail.com>
2022-04-07 21:16:34 +0900

    2-3ルート情報を追加する

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+$html = <<<EOF
+<html>
+<head>
+<title>Hello</title>
+<style>
+body {font-size:16pt; color:#999;}
+h1 {font-size:100pt; text-align:right; color:#eee;
+    margin:-40px 0px -50px 0px;}
+</style>
+</head>
+<body>
+    <h1>Hello</h1>
+    <p>This is sample page.</p>
+    <p>これは、サンプルで作ったページです。</p>
+</body>
+</html>
+EOF;
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello',function() {
-    return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
+Route::get('hello',function() use ($html) {
+    return $html;
 });
+
+// Route::get('hello',function() {
+//     return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
+// });
