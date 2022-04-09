babe16c896173beb544fba2041cb06dcf31a7712
zunema <yxxa.abc@gmail.com>
2022-04-09 22:10:09 +0900

    アクションを追加

--- app/Http/Controllers/HlloController.php
+++ app/Http/Controllers/HlloController.php
@@ -7,6 +7,21 @@ use Illuminate\Http\Request;
 class HlloController extends Controller
 {
     public function index() {
-        
+        return <<<EOF
+        <html>
+            <head>
+                <title>Hello/index</title>
+                <style>
+                body { font-size:16pt; color:#999; }
+                h1 { font-size:1000pt; text-align:right; color:#eee;
+                    margin:-40px 0px -50px 0px; }
+                </style>
+            </head>
+            <body>
+                <h1>Index</h1>
+                <p>これは、Helloコントローラのindexアクションです </p>
+            </body>
+        </html>
+        EOF;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,28 +20,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('hello/{m}/{s}/{g}',function($m, $s,$g) {
-Route::get('hello/{m?}/{s?}/{g?}',function($m='no message.', $s='no 2.', $g='no 3.') {
-
-$html = <<<EOF
-<html>
-<head>
-<title>Hello</title>
-<style>
-body {font-size:16pt; color:#999;}
-h1 {font-size:100pt; text-align:right; color:#eee;
-    margin:-40px 0px -50px 0px;}
-</style>
-</head>
-<body>
-    <h1>Hello</h1>
-    <p>{$m}</p>
-    <p>{$s}</p>
-    <p>{$g}</p>
-    <p>これは、サンプルで作ったページです。</p>
-</body>
-</html>
-EOF;
-
-    return $html;
-});
+Route::get('hello', 'HelloController@index');
