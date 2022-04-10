77353bcf00dac7c89c90ff72d1e10046db3c3398
zunema <yxxa.abc@gmail.com>
2022-04-10 16:01:17 +0900

    2-15.RequestおよびResponse

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,44 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-// // 2-10複数アクションの利用
-// global $head, $style, $body, $end;
-// $head = '<html><head>';
-// $style = <<<EOF
-// <style>
-// body { font-size:16pt; color:#999; }
-// h1 { font-size:100pt; text-align:right; color:#eee;
-//     margin:-40px 0px -50px 0px; }
-// </style>
-// EOF;
-// $body = '</head></body>';
-// $end = '</body></html>';
-// function tag($tag, $txt) {
-//     return "<{$tag}>" . $txt . "</{$tag}>";
-// }
+use Illuminate\Http\Response;  // 2-15.RequestおよびResponse
 
 class HelloController extends Controller
 {
-    // 2-13.HelloControllerをシングルアクション化
-    public function __invoke() {
-        return <<<EOF
+    // 2-15.RequestおよびResponse
+    public function index(Request $request, Response $response) {
+        $html <<<EOF
         <html>
             <head>
-                <title>Hello</title>
+                <title>Hello/Index</title>
                 <style>
                     body { font-size:16pt; color:#999; }
-                    h1 { font-size:30pt; text-align:right; color:#eee;
-                        margin:-15px 0px 0px 0px; }
+                    h1 { font-size:120pt; text-align:right; color:#fafafa;
+                        margin:-50px 0px 120px 0px; }
                 </style>
             </head>
             <body>
-                <h1>Single Action</h1>
-                <p>これは、シングルアクションコントローラのアクションです。</p>
+                <h1>Hello</h1>
+                <h3>Request</h3>
+                <pre>{$request}</pre>
+                <h3>Response</h3>
+                <pre>{$response}</pre>
             </body>
         </html>
         EOF;
+            $response->setContent($html);
+            return $response;
     }
+
     // // 2-10複数アクションの利用
     // public function index() {
     //    global $head, $style, $body, $end;
