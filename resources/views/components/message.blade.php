058fdf3c2142f86e7a988511563e6c88be6d90be
zunema <yxxa.abc@gmail.com>
2022-04-12 13:37:09 +0900

    3-31.@eachによるコレクションビュー

--- resources/views/components/message.blade.php
+++ resources/views/components/message.blade.php
@@ -18,6 +18,6 @@
   }
 </style>
 <div class="message">
-  <p class="msg_title">{{ $msg_title1 }}</p>
+  <p class="msg_title">{{ $msg_title }}</p>
   <p class="msg_content">{{ $msg_content }}</p>
 </div>
