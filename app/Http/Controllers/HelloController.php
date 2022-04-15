be9c694db2a090954ce62cb77b84f8b324782a5f
zunema <yxxa.abc@gmail.com>
2022-04-16 03:58:13 +0900

    4-31.HelloValidatorのルールを使用する

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,54 +9,66 @@ use Validator;  //4-24.バリbariデータの作成
 
 class HelloController extends Controller
 {
-    // 4-25.クエリー文字列にバリデータを適用する
+    // 4-31.HelloValidatorのルールを使用する
     public function index(Request $request)
     {
-        $validator = Validator::make($request->query(), [
-            'id' => 'required',
-            'pass' => 'required',
-        ]);
-        if ($validator->fails()) {
-            $msg = 'クエリーに問題があります。';
-        } else {
-            $msg = 'ID/PASSを受け付けました。フォームを入力ください。';
-        }
-        return view('hello.index', ['msg' => $msg,]);
+        return view('hello.index', ['msg' => 'フォームを入力ください。']);
     }
 
-    // 4-26.エラーメッセージのカスタマイズ
-    public function post(Request $request)
+    // 4-31.HelloValidatorのルールを使用する
+    public function post(HelloRequest $request)
     {
-        $rules = [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric',
-        ];
-        $messages = [
-            'name.required' => '名前は必ず入力してください。',
-            'mail.email' => 'メールアドレスが必要です。',
-            'age.numeric' => '年齢を整数で記入ください。',
-            'age.min' => '年齢は0歳以上で記入ください。',
-            'age.max' => '年齢は200際以下で記入ください。',
-        ];
-
-        $validator = Validator::make($request->all(), $rules, $messages);
-
-        $validator->sometimes('age', 'min:0', function($input) {
-            return is_numeric($input->age);
-        });
-
-        $validator->sometimes('age', 'max:200', function($input) {
-            return is_numeric($input->age);
-        });
-
-        if ($validator->fails()) {
-            return redirect('/hello')
-                    ->withErrors($validator)
-                    ->withInput();
-        }
         return view('hello.index', ['msg' => '正しく入力されました！']);
     }
+
+    // // 4-25.クエリー文字列にバリデータを適用する
+    // public function index(Request $request)
+    // {
+    //     $validator = Validator::make($request->query(), [
+    //         'id' => 'required',
+    //         'pass' => 'required',
+    //     ]);
+    //     if ($validator->fails()) {
+    //         $msg = 'クエリーに問題があります。';
+    //     } else {
+    //         $msg = 'ID/PASSを受け付けました。フォームを入力ください。';
+    //     }
+    //     return view('hello.index', ['msg' => $msg,]);
+    // }
+
+    // // 4-26.エラーメッセージのカスタマイズ
+    // public function post(Request $request)
+    // {
+    //     $rules = [
+    //         'name' => 'required',
+    //         'mail' => 'email',
+    //         'age' => 'numeric',
+    //     ];
+    //     $messages = [
+    //         'name.required' => '名前は必ず入力してください。',
+    //         'mail.email' => 'メールアドレスが必要です。',
+    //         'age.numeric' => '年齢を整数で記入ください。',
+    //         'age.min' => '年齢は0歳以上で記入ください。',
+    //         'age.max' => '年齢は200際以下で記入ください。',
+    //     ];
+
+    //     $validator = Validator::make($request->all(), $rules, $messages);
+
+    //     $validator->sometimes('age', 'min:0', function($input) {
+    //         return is_numeric($input->age);
+    //     });
+
+    //     $validator->sometimes('age', 'max:200', function($input) {
+    //         return is_numeric($input->age);
+    //     });
+
+    //     if ($validator->fails()) {
+    //         return redirect('/hello')
+    //                 ->withErrors($validator)
+    //                 ->withInput();
+    //     }
+    //     return view('hello.index', ['msg' => '正しく入力されました！']);
+    // }
 
 
     // // 4-22.バリデーションを利用する（フォームリクエスト）
