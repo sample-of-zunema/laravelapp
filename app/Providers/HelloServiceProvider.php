ced114492db3a18ea8533636a49914863e1bcc3a
zunema <yxxa.abc@gmail.com>
2022-04-16 04:22:27 +0900

    4-35.Myruleを使ってみる

--- app/Providers/HelloServiceProvider.php
+++ app/Providers/HelloServiceProvider.php
@@ -26,9 +26,15 @@ class HelloServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $validator = $this->app['validator'];
-        $validator->resolver(function($translator, $data, $rules, $messages) {
-            return new HelloValidator($translator, $data, $rules, $messages);
+        // 4-32.Validator::extendを利用する
+        Validator::extend('hello', function($attribute, $value, $parameters, $validator) {
+            return $value % 2 == 0;
         });
+
+        // // 4-29.HelloValidatorを組み込む
+        // $validator = $this->app['validator'];
+        // $validator->resolver(function($translator, $data, $rules, $messages) {
+        //     return new HelloValidator($translator, $data, $rules, $messages);
+        // });
     }
 }
