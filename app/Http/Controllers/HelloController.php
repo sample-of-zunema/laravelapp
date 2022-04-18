4cab178f1c552897c4e591b1257387b84c341ca0
zunema <yxxa.abc@gmail.com>
2022-04-18 22:16:27 +0900

    5-11.データベースの利用（アップデート文）

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -31,9 +31,9 @@ class HelloController extends Controller
     public function create(Request $request)
     {
         $param = [
-            'name' => $request->name;
-            'mail' => $request->mail;
-            'age' => $request->age;
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
         ];
         DB::insert('insert into people (name, mail, age) values (:name, :mail, :age)', $param);
         return redirect('/hello');
