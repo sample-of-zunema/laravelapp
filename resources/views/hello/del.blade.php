5901c6776fb21a6ccd9acf5896a27c6902ed4153
zunema <yxxa.abc@gmail.com>
2022-04-20 01:12:27 +0900

    5-31.マイグレーション、テーブル生成

--- resources/views/hello/del.blade.php
+++ resources/views/hello/del.blade.php
@@ -14,7 +14,7 @@
       <input type="hidden" name="id" value="{{ $form->id }}">
       <tr><th>name: </th><td>{{ $form->name }}</td></tr>
       <tr><th>mail: </th><td>{{ $form->mail }}</td></tr>
-      <tr><th>age: </th><td>{{ $form->age }}"</td></tr>
+      <tr><th>age: </th><td>{{ $form->age }}</td></tr>
       <tr><th></th><td><input type="submit" value="send"></td></tr>
     </table>
   </form>
