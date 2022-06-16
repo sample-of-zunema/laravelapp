5b9d2b78a355b135f01492089dfe3afe8ea90675
zunema <yxxa.abc@gmail.com>
2022-06-16 22:16:29 +0900

    7-4.ユーザ認証

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -19,7 +19,7 @@
       @if (Auth::check())
             <p>USER: {{ $user->name . '（' . $user->email . '）' }}</p>
       @else
-            <p>※ログインしていません。（<a href="/login">ログイン</a><a href="/register">登録</a>）</p>
+            <p>※ログインしていません。（<a href="/login">ログイン</a>｜<a href="/register">登録</a>）</p>
       @endif
       <table>
             <tr>
