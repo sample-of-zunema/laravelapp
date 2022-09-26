6fafb485c132eac45e25297610a9a3524b04832e
zunema <yxxa.abc@gmail.com>
2022-09-27 03:56:00 +0900

    7-5.ユニットテスト

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;  // 2-15.RequestおよびResponse
 use App\Http\Requests\HelloRequest;  // 4-22.バリデーションのフォームリクエスト
 use Validator;  //4-24.バリbariデータの作成
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB;// 9
 use App\Models\Person;
 use illuminate\Support\Facades\Auth;
 
